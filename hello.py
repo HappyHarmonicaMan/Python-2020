150c950216440cc638c5cace06130e2873591610
Jacky Lee <bluseharp@naver.com>
2020-11-08 22:20:05 +0900

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+# Clear the terminal screen
+import os
+os.system('clear')
+
+
+
+
+
+first_name = 'John'
+age = 41
+fav_pizza = {
+    "John": "Pepperoni",
+    "Bob": "Mushroom",
+    "Mary": "Cheese"
+}
+
+name = True
+
+print(name) 
+
+
